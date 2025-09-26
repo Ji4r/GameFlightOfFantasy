923ab05487be1674e4e383b2eb75bb9b3c8b9dc2
Zikais <lilepak@mail.ru>
2025-09-26 22:52:07 +0300

    Create project
    Create FindSound

---
+++ Assets/aPOFromPsychologist/Script/FindSound/Slot/SlotUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SlotUI : Slot
+{
+    public override void OnDrop(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag != null)
+        {
+            var otherItemTransform = eventData.pointerDrag.transform;
+            otherItemTransform.SetParent(transform);
+            otherItemTransform.localPosition = Vector3.zero;
+        }
+    }
+}
